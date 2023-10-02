5ffb277e25ec9800df404d3b1fd15e6bbb10db87
nathanieee <nthn145975@gmail.com>
2023-10-02 17:33:33 +0800

    wanted to create a member req and cont.

--- internal/controllers/requests/member.go
+++ internal/controllers/requests/member.go
@@ -15,4 +15,13 @@ type (
 		Gender      consttypes.Gender `json:"gender" gorm:"not null" binding:"required" example:"Male"`
 		DateOfBirth time.Time         `json:"date" gorm:"not null" binding:"required" example:"2000-10-20"`
 	}
+
+	UpdateMemberRequest struct {
+		Height      float64           `json:"height" gorm:"not null" binding:"required" example:"100"`
+		Weight      float64           `json:"weight" gorm:"not null" binding:"required" example:"150"`
+		FirstName   string            `json:"firstName" gorm:"not null" binding:"required" example:"Jonathan"`
+		LastName    string            `json:"lastName" gorm:"not null" binding:"required" example:"Vince"`
+		Gender      consttypes.Gender `json:"gender" gorm:"not null" binding:"required" example:"Male"`
+		DateOfBirth time.Time         `json:"date" gorm:"not null" binding:"required" example:"2000-10-20"`
+	}
 )
