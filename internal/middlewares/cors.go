6d48bc0118f3bdba59ce528bc012da4a6d2fa2af
nathanieee <nthn145975@gmail.com>
2024-08-29 19:08:42 +0800

    update: cors allow control headers.

--- internal/middlewares/cors.go
+++ internal/middlewares/cors.go
@@ -11,7 +11,7 @@ func CORSMiddleware() gin.HandlerFunc {
 		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		ctx.Writer.Header().Set("Access-Control-Max-Age", "86400")
 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, api_key, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Authorization")
+		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, api_key, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Authorization, Access-Token, Refresh-Token")
 		ctx.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length, Access-Token, Refresh-Token")
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		ctx.Writer.Header().Set("Cache-Control", "no-cache")
